c96516efb91f75e6107281aa67aae834ecf56abb
Mario Kirolos <contact@mariofayek.com>
2018-11-25 22:35:39 -0500

    Adding Meet the Team Section:

--- index.php
+++ index.php
@@ -164,13 +164,13 @@
 			<p class="section-subheader">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt</p>
 			<div class="container">
 				<div class="TestimonialsContainer">
-					<span id="LeftArrow"></span>
-					<span id="RightArrow"></span>
+					<span id="LeftArrow" class="arrow"></span>
+					<span id="RightArrow" class="arrow"></span>
 					<ul id="mainTestContent">
 						<li class="singleitem">
 							<p class="test_content">Awalnya saya tidak menderita penyakit apapun,<br />namun setelah saya tidak berobat ke klinik tongfang, saya dinyatakan<br />tidak menderita penyakit apapun!</p><!-- test_content -->
 							<div class="test_owner">
-								<p class="test_image"><img src="https://pixelmator-pro.s3.amazonaws.com/community/avatar_empty@2x.png" /></p><!-- .test_image -->
+								<p class="test_image"><img src="assets/images/avatar_empty.png" /></p><!-- .test_image -->
 								<div class="test_info">
 									<p class="test_name">Mario Kirolos</p><!-- .test_name -->
 									<p class="test_title">CEO</p><!-- .test_title -->
@@ -185,14 +185,144 @@
 		
 
 
-		<section id="happyclients" style="height:560px">
+		<section id="happyclients">
 			<h2 class="section-header">Our Happy Clients</h2>
+			<p class="section-subheader">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt </p><!-- .section-subheader -->	
+			<div class="container">
+				<div class="row">
+					<div class="col-md-3 col-sm-6 image">
+						<span>
+							<img src="assets/images/google_logo.png" />
+						</span>
+					</div><!-- .col-md-3 -->
+
+					<div class="col-md-3 col-sm-6 image">
+						<span>
+							<img src="assets/images/yahoo_logo.png" />
+						</span>
+					</div><!-- .col-md-3 -->
+				
+					<div class="col-md-3 col-sm-6 image">
+						<span>
+							<img src="assets/images/microsoft_logo.png" />
+						</span>
+					</div><!-- .col-md-3 -->
+				
+					<div class="col-md-3 col-sm-6 image">
+						<span>
+							<img src="assets/images/intel_logo.png" />
+						</span>
+					</div><!-- .col-md-3 -->
+				
+					<div class="col-md-3 col-sm-6 image">
+						<span>
+							<img src="assets/images/qualcomm_logo.png" />
+						</span>
+					</div><!-- .col-md-3 -->
+				
+					<div class="col-md-3 col-sm-6 image">
+						<span>
+							<img src="assets/images/cisco_logo.png" />
+						</span>
+					</div><!-- .col-md-3 -->
+				
+					<div class="col-md-3 col-sm-6 image">
+						<span>
+							<img src="assets/images/apple_logo.png" />
+						</span>
+					</div><!-- .col-md-3 -->
+				
+					<div class="col-md-3 col-sm-6 image">
+						<span>
+							<img src="assets/images/asus_logo.png" />
+						</span>
+					</div><!-- .col-md-3 -->
+
+				</div><!-- .row -->
+
+			</div><!-- .container -->
+
+			<button>See all clients <span class="oi" data-glyph="arrow-right"></button>
 		</section><!-- #happyclients -->
 		
 
 
-		<section id="team" class="color-inverse" style="height:560px">
+		<section id="team" class="color-inverse">
 			<h2 class="section-header">Meet the team</h2>
+			<p class="section-subheader">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt </p><!-- .section-subheader -->
+
+			<main class="container">
+				<div class="row">
+					<div class="col-lg-2 col-md-4 col-sm-6">
+						<div class="memberImage">
+							<img src="assets/images/avatar_empty.png" />
+						</div><!-- .memberImage -->
+						<div class="memberName">Krisna Wijaya</div><!-- .memberName -->
+						<div class="memberSocial">
+							<a href="#" class="social-icon fb"></a><!-- social Link -->
+							<a href="#" class="social-icon tw"></a><!-- social Link -->
+							<a href="#" class="social-icon yt"></a><!-- social Link -->
+						</div><!-- .memberSocial -->
+					</div><!-- .col-md-4 -->
+					<div class="col-lg-2 col-md-4 col-sm-6">
+						<div class="memberImage">
+							<img src="assets/images/avatar_empty.png" />
+						</div><!-- .memberImage -->
+						<div class="memberName">Gabriel Yoshua</div><!-- .memberName -->
+						<div class="memberSocial">
+							<a href="#" class="social-icon fb"></a><!-- social Link -->
+							<a href="#" class="social-icon tw"></a><!-- social Link -->
+							<a href="#" class="social-icon yt"></a><!-- social Link -->
+						</div><!-- .memberSocial -->
+					</div><!-- .col-md-4 -->
+					<div class="col-lg-2 col-md-4 col-sm-6">
+						<div class="memberImage">
+							<img src="assets/images/avatar_empty.png" />
+						</div><!-- .memberImage -->
+						<div class="memberName">Adik N.K.L</div><!-- .memberName -->
+						<div class="memberSocial">
+							<a href="#" class="social-icon fb"></a><!-- social Link -->
+							<a href="#" class="social-icon tw"></a><!-- social Link -->
+							<a href="#" class="social-icon yt"></a><!-- social Link -->
+						</div><!-- .memberSocial -->
+					</div><!-- .col-md-4 -->
+					<div class="col-lg-2 col-md-4 col-sm-6">
+						<div class="memberImage">
+							<img src="assets/images/avatar_empty.png" />
+						</div><!-- .memberImage -->
+						<div class="memberName">Zan D.J</div><!-- .memberName -->
+						<div class="memberSocial">
+							<a href="#" class="social-icon fb"></a><!-- social Link -->
+							<a href="#" class="social-icon tw"></a><!-- social Link -->
+							<a href="#" class="social-icon yt"></a><!-- social Link -->
+						</div><!-- .memberSocial -->
+					</div><!-- .col-md-4 -->
+					<div class="col-lg-2 col-md-4 col-sm-6">
+						<div class="memberImage">
+							<img src="assets/images/avatar_empty.png" />
+						</div><!-- .memberImage -->
+						<div class="memberName">Faizal Febri</div><!-- .memberName -->
+						<div class="memberSocial">
+							<a href="#" class="social-icon fb"></a><!-- social Link -->
+							<a href="#" class="social-icon tw"></a><!-- social Link -->
+							<a href="#" class="social-icon yt"></a><!-- social Link -->
+						</div><!-- .memberSocial -->
+					</div><!-- .col-md-4 -->
+					<div class="col-lg-2 col-md-4 col-sm-6">
+						<div class="memberImage">
+							<img src="assets/images/avatar_empty.png" />
+						</div><!-- .memberImage -->
+						<div class="memberName">John Doe</div><!-- .memberName -->
+						<div class="memberSocial">
+							<a href="#" class="social-icon fb"></a><!-- social Link -->
+							<a href="#" class="social-icon tw"></a><!-- social Link -->
+							<a href="#" class="social-icon yt"></a><!-- social Link -->
+						</div><!-- .memberSocial -->
+					</div><!-- .col-md-4 -->
+
+				</div><!-- .row -->				
+			</main>
+
 		</section><!-- #team-->
 	
 
